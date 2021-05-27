e2f2b2fd9c023b9f9dd2ad8e1989da74ff8fc268
gustn3965 <gustn3965@gmail.com>
2021-05-27 18:36:05 +0900

    implement CustomHaspMap

--- CustomHashMap/CustomHashMap.swift
+++ CustomHashMap/CustomHashMap.swift
@@ -5,6 +5,112 @@
 //  Created by hyunsu on 2021/05/27.
 //
 
-class CustomHashMap {
 
+struct CustomHashMap<Value> {
+
+    typealias Key = Int
+    
+    struct Bucket {
+        var key: Key?
+        var value: Value?
+    }
+    
+    // MARK: - Property
+    var storage = [Bucket?]()
+    var capacity: Int
+    var usage = 0
+    var totalUsage: Int = 0
+    
+    // MARK: - Method
+    init(_ capacity: Int ) {
+        self.capacity = capacity
+        self.storage = Array(repeating: nil, count: capacity)
+    }
+    
+    subscript(key: Key) -> Value? {
+        mutating get {
+            let (found, hashedKey) = find(&storage, key)
+            if found {
+                return storage[hashedKey]?.value
+            } else {
+                return nil
+            }
+        }
+        set {
+            let (found, hashedKey) = find(&storage, key)
+            if let value = newValue {
+                if found {
+                    update(hashedKey, value)
+                } else {
+                    insert(hashedKey: hashedKey, Bucket(key: key, value: value))
+                }
+            } else {
+                if found {
+                    remove(hashedKey)
+                }
+            }
+        }
+    }
+    
+    // Find hashedKey.
+    // if found is true, hashedKey is that
+    // if found is false, hashedKey is empty key.
+    func find(_ storage: inout [Bucket?], _ key: Key) -> (found: Bool, hashedKey: Int) {
+        var hashed = getKey(key: key)
+        var findKey: Int = 0
+        var count = 0
+        
+        while count < capacity {
+            if storage[hashed]?.key == key ||
+                storage[hashed] == nil {
+                findKey = hashed
+                break
+            }
+            hashed = hashed == capacity-1 ? 0 : hashed+1
+            count += 1
+        }
+
+        let find = storage[hashed]?.key == key
+        return (find, findKey)
+    }
+    
+    mutating func remove(_ hashedKey: Int) {
+        storage[hashedKey]?.key = nil
+        usage -= 1
+        totalUsage += 1 
+        if totalUsage > capacity/2 {
+            resize()
+        }
+    }
+    
+    mutating func update(_ hashedKey: Int, _ value: Value ) {
+        storage[hashedKey]?.value = value
+    }
+    
+    mutating func insert( hashedKey: Int, _ bucket: Bucket ) {
+        storage[hashedKey] = bucket
+        usage += 1
+        totalUsage += 1
+        if totalUsage > capacity/2 {
+            resize()
+        }
+    }
+    
+    func getKey(key: Int) -> Int {
+        return key%capacity
+    }
+    
+    mutating func resize() {
+        var new: [Bucket?] = Array(repeating: nil, count: capacity*2)
+        let before = capacity
+        capacity*=2
+        for i in 0..<before {
+            if let key = storage[i]?.key {
+                let (_, hashed) = find(&new, key)
+                new[hashed] = storage[i]
+            }
+        }
+        storage = new
+        totalUsage = usage
+    }
 }
---
+++ HashTest/HashTest.swift
@@ -0,0 +1,84 @@
+//
+//  HashTest.swift
+//  HashTest
+//
+//  Created by hyunsu on 2021/05/27.
+//
+
+import XCTest
+@testable import CustomHashMap
+
+class HashTest: XCTestCase {
+
+    func test_10개사이즈로_key로1에넣으면_index1에_key1가있어야한다() throws {
+        var dict = CustomHashMap<Int>(10)
+        dict[1] = 1
+        XCTAssertTrue(dict.storage[1]?.key == 1)
+    }
+    
+    func test_10개사이즈로_key로1에넣고_key11를넣는다면_index2에_key11가있어야한다() throws {
+        var dict = CustomHashMap<Int>(10)
+        dict[1] = 1
+        dict[11] = 11
+        XCTAssertTrue(dict.storage[2]?.key == 11)
+    }
+    
+    func test_10개사이즈로_key로1에넣고_key11를넣고_key11를삭제하고_key2를넣는다면_index3에_key2가있어야한다() {
+        var dict = CustomHashMap<Int>(10)
+        dict[1] = 1
+        dict[11] = 11
+        dict[11] = nil
+        dict[2] = 2
+        XCTAssertTrue(dict.storage[3]?.key == 2)
+    }
+
+    func test_10개사이즈로_key로1에넣고_key11를넣고_key11를삭제하면_index2에_key가_nil이여야한다() {
+        var dict = CustomHashMap<Int>(10)
+        dict[1] = 1
+        dict[11] = 11
+        dict[11] = nil
+        XCTAssertNotNil(dict.storage[2]!.key == nil )
+    }
+    
+    func test_10개사이즈로_key0에넣고_key9에넣고_key99를넣는다면_index1에_key99가있어야한다() {
+        let size = 10
+        var dict = CustomHashMap<Int>(size)
+        dict[0] = 0
+        dict[9] = 9
+        dict[99] = 99
+        XCTAssertTrue(dict.storage[1]!.key == 99)
+    }
+    
+    func test_10개사이즈로_6개를넣었다면_resizing되어_capacity가_2배되어야한다() {
+        let size = 10
+        var dict = CustomHashMap<Int>(size)
+        dict[1] = 1
+        dict[2] = 2
+        dict[3] = 3
+        dict[4] = 4
+        dict[5] = 5
+        dict[6] = 6
+        XCTAssertTrue(dict.capacity == size*2)
+    }
+    func test_10개사이즈로_삽입3번_삭제3번했다면_resizing되어_capacity가_2배되어야한다() {
+        let size = 10
+        var dict = CustomHashMap<Int>(size)
+        dict[1] = 1
+        dict[2] = 2
+        dict[3] = 3
+        dict[1] = nil
+        dict[2] = nil
+        dict[3] = nil
+        XCTAssertTrue(dict.capacity == size*2)
+    }
+
+    func test_10개사이즈로_삽입21번한다면_resizing2번되어_capacity가_80이되어야한다() {
+        let size = 10
+        var dict = CustomHashMap<Int>(size)
+        for i in 0..<21 {
+            dict[i] = i
+        }
+        XCTAssertTrue(dict.capacity == 80, "\(dict.capacity)")
+    }
+
+}
